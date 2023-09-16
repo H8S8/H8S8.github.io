9559c4be4abff1899b57c62cc6101d983629d9db
H7S7 <hannah.847@btinternet.com>
2023-09-16 10:37:51 +0100

    Show selected navbar option

--- nav.js
+++ nav.js
@@ -1,4 +1,12 @@
-function selectTab(id){
-    var button = document.getElementById(id);
-    button.className = "selected";
+function selectCurrentPage(){
+    
+    //Check all navbar options to see which is the current page
+    let navLabels = ["Home", "AboutMe", "CV", "CodingProjects", "Dissertation", "LaboratoryReports"];
+
+    for(let i=0; i<navLabels.length; i++){
+        var page = document.getElementById(navLabels[i]);
+        if(page.href == window.location.href){
+            page.className = "current";
+        }
+    }
 }
